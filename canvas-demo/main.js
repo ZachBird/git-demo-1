5da5e47cbb7cfff1d0190e4965d6517d027e03cf
可囿可无 <546439325@qq.com>
2017-11-14 23:41:13 +0800

    canvas初涉...很蠢的橡皮擦，看看怎么解决

---
+++ canvas-demo/main.js
@@ -0,0 +1,140 @@
+
+/*======  用增加元素的方式画，无法连续  ====== */
+
+// const canvas = document.querySelector('#canvas')
+// let painting = false //画画状态
+
+// //按下鼠标
+// canvas.onmousedown = function(e) {
+//     painting = true
+//     let x = e.clientX
+//     let y = e.clientY
+//     let divA = document.createElement('div')
+//     divA.style = `width: 6px; height: 6px; 
+//     background: black; border-radius: 3px; 
+//     position: absolute; left: ${x - 10}px; top: ${y + 4}px;`
+//     canvas.appendChild(divA)
+// }
+
+// //鼠标移动
+// canvas.onmousemove = function(e) {
+//     if(painting) {
+//         let x = e.clientX
+//         let y = e.clientY
+//         let divA = document.createElement('div')
+//         divA.style = `width: 6px; height: 6px; 
+//         background: black; border-radius: 3px; 
+//         position: absolute; left: ${x - 10}px; top: ${y + 4}px;`
+//         canvas.appendChild(divA)
+//     }
+// }
+
+// //松开鼠标
+// canvas.onmouseup = function(e) {
+//     painting = false
+// }
+
+/*====== Canvas ======*/
+const paintingArea = document.querySelector('#painting-area')
+const context = paintingArea.getContext('2d')//获取二次元的上下文
+const eraser = document.querySelector('#eraser')
+let using = false
+let lastPoint = {x: null, y: null}//存上一次画的最后一个点的位置坐标
+
+function setSize() {
+    const pageWidth = document.documentElement.clientWidth
+    const pageHeight = document.documentElement.clientHeight
+    paintingArea.width = pageWidth
+    paintingArea.height = pageHeight
+}
+
+setSize()
+window.onresize = function() {
+    setSize()
+}
+
+/*====== Canvas 画各种图形 ======*/
+// context.strokeStyle = 'blue'
+// context.strokeRect(10, 10, 100, 100)
+// context.fillStyle = 'red'
+// context.fillRect(10, 10, 100, 100)
+
+// context.clearRect(50, 50, 10, 10)
+
+// context.fillStyle = 'lightpink'
+// context.beginPath()
+// context.moveTo(240, 240)
+// context.lineTo(300, 240)
+// context.lineTo(300, 300)
+// context.fill()
+
+/*====== Canvas 画线 ======*/
+
+// context.beginPath()
+// context.moveTo(0, 0)//起点
+// context.lineTo(200, 0)//终点
+// context.lineWidth = 5
+// context.stroke()
+// context.closePath()
+
+function drawCircle(x, y, radius) {
+    context.strockStyle = 'black'
+    context.beginPath()
+    context.arc(x, y, radius, 0, Math.PI*2)
+    context.fill()
+}
+
+function drawLine(x1, y1, x2, y2) {
+    context.beginPath()
+    context.moveTo(x1, y1)//起点
+    context.lineTo(x2, y2)//终点
+    context.lineWidth = 6
+    context.stroke()
+    context.closePath()
+}
+
+let eraserEnabled = false
+eraser.onclick = function() {
+    eraserEnabled = !eraserEnabled
+    if(eraserEnabled) {
+        eraser.textContent = '画笔'
+    }else{
+        eraser.textContent = '擦擦擦'
+    }
+}
+
+
+paintingArea.onmousedown = function(e) {
+    let x = e.clientX
+    let y = e.clientY
+    if(eraserEnabled) {
+        using = true
+        // drawCircle(x, y, 3)
+        context.clearRect(x-5, y-5, 40, 40)
+    }else{
+        using = true
+        lastPoint = {x: x, y: y}
+    }
+}
+
+paintingArea.onmousemove = function(e) {
+    let x = e.clientX
+    let y = e.clientY
+    if(eraserEnabled) {
+        if(using) {
+            context.clearRect(x-5, y-5, 40, 40)
+        }
+    }else{
+        if(using) {
+            let newPoint = {x: x, y: y}
+            drawCircle(x, y, 3)
+            drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+            lastPoint = newPoint
+        }
+    }
+   
+}
+
+paintingArea.onmouseup = function(e) {
+    using = false
+}
--- nav-demo/main.js
+++ nav-demo/main.js
@@ -1,3 +1,4 @@
+//1.初始化数据
 let keys = [
     ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', ], 
     ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ],
@@ -21,45 +22,87 @@ let hash = {
     'x': 'xiedaimala.com',
     'm': 'www.mcdonalds.com.cn',
 }
-hashInLocalStorage = JSON.parse(localStorage.getItem('zzz') || 'null')
+//取出loaclStrotage
+let hashInLocalStorage = acfun('zzz')
 if(hashInLocalStorage) {
     hash = hashInLocalStorage
 }
+function acfun(name){
+   return JSON.parse(localStorage.getItem(name) || 'null')
+}
+//2. 生成键盘
 //操作DOM树
-index = 0
-while(index < keys.length) {
-    div1 = document.createElement('div')
-    kbdWrap.appendChild(div1)
-    row = keys[index]
-    index2 = 0
-    while(index2 < row.length){
-        kbd = document.createElement('kbd')
-        kbd.textContent = row[index2]
-        button = document.createElement('button')
+function tag(tagName, attrbutes) {
+    let element = document.createElement(tagName)
+    for(let key in attrbutes) {
+        element.key = attrbutes.key
+    }
+    return element
+}
+
+for(let index = 0; index < keys.length; index++) {
+
+}
+var index = 0
+while(index < keys.length) { //0 1 2
+    var div = tag('div', {className: 'row'})
+    div.className = 'row'
+    kbdWrap.appendChild(div)
+    var row = keys[index]//第一个数组 第二个数组 第三个数组
+    var index2 = 0
+    while(index2 < row.length){// 0-9 0-8 0-6
+        var span = tag('span')
+        span.textContent = row[index2]
+        span.className = 'text'
+
+        var button = tag('button')
         button.textContent = '编辑'
         button.id = row[index2]
-        button.addEventListener('click', (e) => {
-            console.log(e.target.id)
-        })
         button.onclick = function(e) {
-            key = e.target.id
-            x = prompt('aaa')
-            hash[key] = x
+            var button2 = e.target
+            var img2 = button2.previousSibling //拿到前一个节点
+            var key = e.target.id
+            var x = prompt('aaa')
+            hash[key] = x//hash变更
+            img2.src = 'http://' + key +'/favicon.ico'
+            img2.onerror = function(e) {
+                e.target.src = ''//默认icon
+            }
             localStorage.setItem('zzz', JSON.stringify(hash))
             console.log(hash)
         }
+       
+        var img = tag('img')
+        if(hash[row[index2]]){
+            img.src = 'http://' + hash[row[index2]] +'/favicon.ico'
+        }else{
+            img.src = ''//默认的icon
+        }
+        img.onerror = function(e) {
+            e.target.src = ''//默认icon
+        }
+        // button.addEventListener('click', (e) => {
+        //     console.log(e.target.id)
+        // })
+        
+        var kbd = tag('kbd')
+        kbd.className = 'key'
+
+        kbd.appendChild(span)
+        kbd.appendChild(img)
         kbd.appendChild(button)
-        div1.appendChild(kbd)
+        div.appendChild(kbd)
         index2 += 1
     }
     index += 1
 }
+//3.监听键盘
 //添加事件监听
-kbd.addEventListener('keydown', (e) => {
-    console.log('123')
-})
+// kbd.addEventListener('keydown', (e) => {
+//     console.log('123')
+// })
 document.onkeypress = function(e) {
-    key = e.key
-    website = hash[key]
+    var key = e.key
+    var website = hash[key]
     window.open(`http://${website}`, '_blank')
 }
